7991b64a293d685f9c29191555a9744101b405db
MiguelGustavo300 <gustavo.miguel.contato300@gmail.com>
2023-08-07 11:24:01 -0300

    barra lateral fixed

--- src/app/componentes/menu-lateral/menu-lateral.component.ts
+++ src/app/componentes/menu-lateral/menu-lateral.component.ts
@@ -9,6 +9,9 @@ import { SidebarModule } from 'ng-sidebar';
 export class MenuLateralComponent {
   opened = false;
   hide:boolean = false;
+  _closeOnClickOutside: boolean = true;
+  _animate: boolean = true;
+
 
 
   toggleSidebar(){
@@ -16,6 +19,12 @@ export class MenuLateralComponent {
   }
   storeBadgeState(){
     localStorage.setItem('hide', JSON.stringify(this.hide))
+  }
+  _toggleCloseOnClickOutside(): void {
+    this._closeOnClickOutside = !this._closeOnClickOutside;
+  }
+  _toggleAnimate(): void {
+    this._animate = !this._animate;
   }
 
 }
